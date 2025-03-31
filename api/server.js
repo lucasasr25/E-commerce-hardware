401666848e6c4f3a7b8016c4acdf10405652cbfd
Lucas Araujo Silva dos Reis <araujo.luksss09@outlook.com>
2025-03-30 23:09:25 -0300

    Updates

--- api/server.js
+++ api/server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const port = 3000;
 
+// Your routes
 const clientRoutes = require("./src/routes/clientRoutes.js");
 const apiRoutes = require("./src/routes/apiClientRoutes.js");
 const cartRoutes = require("./src/routes/cartRoutes.js");
@@ -13,21 +13,16 @@ app.set("views", path.join(__dirname, "src/views"));
 // Defina o mecanismo de view como EJS
 app.set("view engine", "ejs");
 
-
 // Servindo arquivos estáticos da pasta 'public'
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.json());
+
+// Define your routes
 app.use("/client", clientRoutes);
 app.use("/api", apiRoutes);
 app.use("/", productRoutes);
 app.use("/cart", cartRoutes);
 
-if (process.env.NODE_ENV !== 'test') {
-  app.listen(3000, () => {
-      console.log('Servidor rodando em http://localhost:3000');
-  });
-}
-
-module.exports = app;  // Exporta a instância do app
+// Export the app for Vercel to handle the serverless function
+module.exports = app;
